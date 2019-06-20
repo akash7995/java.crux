13b71f6dab1ff7260ebd93fc3b0dcfc328570ba8
akash7995 <41838912+akash7995@users.noreply.github.com>
2019-06-20 09:23:42 +0530

    Create Pattern08.java

---
+++ Pattern08.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int num = 5;
+        for(int i = 1; i <= num; i++){
+            for(int j = 1; j <= num; j++){
+                if(i==j||j==num-(i-1)){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
